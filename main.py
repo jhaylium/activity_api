557c6e3ffaa8a3ed82d4459c965f229574c65c49
Jeff <jmccain@haylium.com>
2021-04-26 22:30:23 -0400

    added wildcard for sdh to origins

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 origins = ['http://localhost:8000', 'metadata.surveydatahub.com',
            'dev-app.surveydatahub.com', 'http://localhost', 'app.surveydatahub.com',
-           'surveydatahub.com']
+           'surveydatahub.com', '*.surveydatahub.com']
 
 app.add_middleware(
     CORSMiddleware,
